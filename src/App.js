2865722e8586a46a07c1226d9918da63c0935ff8
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-03-24 12:50:06 +0000

    feat: barebones store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Search from './components/Search'
 
 export default function BasicExample() {
   return (
@@ -16,15 +17,8 @@ export default function BasicExample() {
 
         <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Search />} />
           <Route path='about' element={<About />} />
         </Routes>
       </div>
@@ -32,17 +26,7 @@ export default function BasicExample() {
   )
 }
 
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  )
-}
-
+// TODO remove or add your creds to this page maybe
 function About() {
   return (
     <div>
---
+++ src/components/Search.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Search = () => <div>Search for a tv show</div>
+
+export default Search
