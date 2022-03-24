43dac668b6f4705b74bc2ce5df270477f3b5dbf2
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-03-24 17:25:16 +0000

    feat: rough tv show fetch

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { searchForShow } from '../redux/store'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchShows } from '../redux/store'
 
 const Search = () => {
   const searchDispatch = useDispatch()
+  const shows = useSelector((state) => state.hits)
+  const status = useSelector((state) => state.status)
+
+  useEffect(() => {
+    if (status === 'idle') {
+      searchDispatch(fetchShows())
+    }
+  }, [status, searchDispatch])
+
   return (
     <div>
       <div>
         <input
           placeholder='Search for a tvshow'
-          onBlur={(e) => searchDispatch(searchForShow(e.target.value))}
+          onBlur={(e) => searchDispatch(fetchShows(e.target.value))}
         />
       </div>
-      <div>List of shows</div>
+      <div>List of shows: {shows.length}</div>
+      <div>Status: {status}</div>
     </div>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,40 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+// TODO maybe move these to their corresponding folders - slice file
+export const fetchShows = createAsyncThunk(
+  'shows/search',
+  async (searchText = 'test') => {
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchText}`
+    )
+    return response.json()
+  }
+)
 
 const searchSlice = createSlice({
   name: 'search-slice',
   initialState: {
     // TODO handle initial load
     hits: [],
+    status: 'idle',
   },
-  reducers: {
-    searchForShow: (state, { payload }) => {
-      // TODO fill the logic here
-      console.log({ payload })
-      state.hits = []
-    },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchShows.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchShows.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.hits = state.hits.concat(action.payload)
+      })
+      .addCase(fetchShows.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
   },
 })
-
-export const { searchForShow } = searchSlice.actions
 
 const store = configureStore({
   reducer: searchSlice.reducer,
