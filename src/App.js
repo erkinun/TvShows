9381d43480a42fe8b9f81f20a95246ff14b3f266
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-04-01 22:40:47 +0100

    refactor: basename for router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Details from './components/Details'
 import Search from './components/Search'
 
 export default function App() {
   return (
-    <Router>
+    <Router basename='tvshows'>
       <div>
         <Routes>
-          <Route path='/' element={<Navigate replace to='/TvShows' />} />
-          <Route path='/TvShows'>
+          <Route path='/'>
             <Route index element={<Search />} />
             <Route path='details' element={<Details />}>
               <Route path=':showId' element={<Details />} />
