d83fe2d4c2a69a133dfa473ad5f009e5c744a7e1
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-04-04 21:53:23 +0100

    feat: added a site button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,15 +6,18 @@ import './Search.css'
 
 const SearchBar = ({ lastSearch, searchDispatch }) => {
   return (
-    <div className='search-bar'>
-      <svg aria-hidden='true' width='18' height='18' viewBox='0 0 18 18'>
-        <path d='m18 16.5-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5ZM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0Z'></path>
-      </svg>
-      <input
-        defaultValue={lastSearch}
-        placeholder='Search for a TV Show'
-        onChange={(e) => searchDispatch(fetchShows(e.target.value))}
-      />
+    <div className='top-bar'>
+      <div className='brand'>TV Shows</div>
+      <div className='search-bar'>
+        <svg aria-hidden='true' width='18' height='18' viewBox='0 0 18 18'>
+          <path d='m18 16.5-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5ZM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0Z'></path>
+        </svg>
+        <input
+          defaultValue={lastSearch}
+          placeholder='Search for a TV Show'
+          onChange={(e) => searchDispatch(fetchShows(e.target.value))}
+        />
+      </div>
     </div>
   )
 }
