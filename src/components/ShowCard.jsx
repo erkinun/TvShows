760ef983be77980062037fbd8e0ec2f34ce2139a
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-04-30 18:24:10 +0100

    feat: channel name and start end years

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -2,14 +2,21 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { reset } from '../redux/detailSlice'
 
-const ShowCard = ({ show: { name, genres, image, id } }) => {
+const ShowCard = ({
+  show: { name, genres, image, id, network, premiered, ended },
+}) => {
   const resetDispatch = useDispatch()
+  const premieredYear = premiered?.slice(0, 4)
+  const endYear = ended?.slice(0, 4) || 'running'
   return (
     <Link to={`details/${id}`} onClick={() => resetDispatch(reset())} key={id}>
       <div className='show-card-wrapper'>
         <img className='show-card-img' src={image?.medium} alt='show' />
         <div className='search-show-card'>
           <div className='name'>{name}</div>
+          <div className='network'>
+            ({network?.name}, {premieredYear} - {endYear})
+          </div>
           <div className='genre'>{genres.join(', ')}</div>
           <div>{''}</div>
         </div>
