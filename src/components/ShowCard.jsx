71701bf218b5d16767553a95e999227b44139d6e
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-04-30 18:04:08 +0100

    img should look like imdb

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -6,11 +6,13 @@ const ShowCard = ({ show: { name, genres, image, id } }) => {
   const resetDispatch = useDispatch()
   return (
     <Link to={`details/${id}`} onClick={() => resetDispatch(reset())} key={id}>
-      <div className='search-show-card'>
-        <img src={image?.medium} alt='show' />
-        <div className='name'>{name}</div>
-        <div className='genre'>{genres.join(', ')}</div>
-        <div>{''}</div>
+      <div>
+        <img className='show-card-img' src={image?.medium} alt='show' />
+        <div className='search-show-card'>
+          <div className='name'>{name}</div>
+          <div className='genre'>{genres.join(', ')}</div>
+          <div>{''}</div>
+        </div>
       </div>
     </Link>
   )
